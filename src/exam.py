c10ff068fb22e7e187b686374e8d992cc72caf22
mghorbani <m.ghorbani2357@gmail.com>
2022-09-04 09:53:19 +0430

    Initial Commit

--- src/exam.py
+++ src/exam.py
@@ -1,2 +1,18 @@
 class Exam:
-    pass
+    @staticmethod
+    def get_new_exam(exam_name, question_count=10):
+        """
+            Args:
+                exam_name (str): Name of the exam to be created.
+                question_count (int): Count of questions which exam is going to have.
+
+            Return:
+                (Exam): New instance of Exam class.
+        """
+        pass
+
+    def load(self):
+        pass
+
+    def export(self):
+        pass
--- tests/TestExam.py
+++ tests/TestExam.py
@@ -1,5 +1,6 @@
 import types
 import unittest
+import uuid
 from distutils.version import LooseVersion
 from unittest import TestCase
 
@@ -11,16 +12,18 @@ class TestExamPackage(TestCase):
     @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.1"), "demonstrating skipping")
     def test_attributes(self):
         exam = Exam()
-        exam_arbitrary_attributes = [
-            'exam_id',
-            'questions',
-            'answers',
-            'keys',
-            'name',
-        ]
+        exam_arbitrary_attributes = {
+            'exam_id': str,
+            'questions': list,
+            'answers': list,
+            'keys': list,
+            'name': str,
+        }
 
         for attribute in exam_arbitrary_attributes:
             self.assertIn(attribute, dir(exam), f'`{attribute}` was not found in Exam class attributes.')
+            if not isinstance(getattr(exam, attribute), exam_arbitrary_attributes.get(attribute)):
+                raise AttributeError(f'`{attribute}` isn\'t {exam_arbitrary_attributes.get(attribute)}.')
 
     @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.1"), "demonstrating skipping")
     def test_methods(self):
@@ -44,3 +47,23 @@ class TestExamPackage(TestCase):
         for method in exam_arbitrary_static_methods:
             if not isinstance(getattr(exam, method), types.FunctionType):
                 raise AttributeError(f'`{method}` method isn\'t static.')
+
+    @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.2"), "demonstrating skipping")
+    def test_init_method(self):
+        pass
+
+    @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.2"), "demonstrating skipping")
+    def test_get_new_exam_method(self):
+        new_exam = Exam.get_new_exam('test_exam')
+        self.assertEqual(10, len(new_exam.questions))
+        self.assertEqual(10, len(new_exam.answers))
+        self.assertEqual(10, len(new_exam.keys))
+        uuid.UUID(new_exam.exam_id)
+
+    @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.2"), "demonstrating skipping")
+    def test_load_method(self):
+        pass
+
+    @unittest.skipIf(LooseVersion(run.__version__) < LooseVersion("0.0.2"), "demonstrating skipping")
+    def test_export_method(self):
+        pass
